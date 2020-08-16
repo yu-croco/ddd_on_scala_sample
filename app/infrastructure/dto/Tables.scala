1acc1598d587e6fc8f33004a26cc98bd3fbe4bd1
yu-croco <yuki.kita22@gmail.com>
2020-08-16 15:47:57 +0900

    codegen

---
+++ app/infrastructure/dto/Tables.scala
@@ -0,0 +1,133 @@
+package dto
+// AUTO-GENERATED Slick data model
+/** Stand-alone Slick data model for immediate use */
+object Tables extends {
+  val profile = slick.jdbc.PostgresProfile
+} with Tables
+
+/** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
+trait Tables {
+  val profile: slick.jdbc.JdbcProfile
+  import profile.api._
+  import slick.model.ForeignKeyAction
+  // NOTE: GetResult mappers for plain SQL are only generated for tables where Slick knows how to map the types of all columns.
+  import slick.jdbc.{GetResult => GR}
+
+  /** DDL for all tables. Call .create to execute. */
+  lazy val schema: profile.SchemaDescription = PlayEvolutions.schema ++ PlayEvolutionsLock.schema ++ Tasks.schema ++ Users.schema
+  @deprecated("Use .schema instead of .ddl", "3.0")
+  def ddl = schema
+
+  /** Entity class storing rows of table PlayEvolutions
+   *  @param id Database column id SqlType(int4), PrimaryKey
+   *  @param hash Database column hash SqlType(varchar), Length(255,true)
+   *  @param appliedAt Database column applied_at SqlType(timestamp)
+   *  @param applyScript Database column apply_script SqlType(text), Default(None)
+   *  @param revertScript Database column revert_script SqlType(text), Default(None)
+   *  @param state Database column state SqlType(varchar), Length(255,true), Default(None)
+   *  @param lastProblem Database column last_problem SqlType(text), Default(None) */
+  case class PlayEvolutionsRow(id: Int, hash: String, appliedAt: java.sql.Timestamp, applyScript: Option[String] = None, revertScript: Option[String] = None, state: Option[String] = None, lastProblem: Option[String] = None)
+  /** GetResult implicit for fetching PlayEvolutionsRow objects using plain SQL queries */
+  implicit def GetResultPlayEvolutionsRow(implicit e0: GR[Int], e1: GR[String], e2: GR[java.sql.Timestamp], e3: GR[Option[String]]): GR[PlayEvolutionsRow] = GR{
+    prs => import prs._
+    PlayEvolutionsRow.tupled((<<[Int], <<[String], <<[java.sql.Timestamp], <<?[String], <<?[String], <<?[String], <<?[String]))
+  }
+  /** Table description of table play_evolutions. Objects of this class serve as prototypes for rows in queries. */
+  class PlayEvolutions(_tableTag: Tag) extends profile.api.Table[PlayEvolutionsRow](_tableTag, "play_evolutions") {
+    def * = (id, hash, appliedAt, applyScript, revertScript, state, lastProblem) <> (PlayEvolutionsRow.tupled, PlayEvolutionsRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = ((Rep.Some(id), Rep.Some(hash), Rep.Some(appliedAt), applyScript, revertScript, state, lastProblem)).shaped.<>({r=>import r._; _1.map(_=> PlayEvolutionsRow.tupled((_1.get, _2.get, _3.get, _4, _5, _6, _7)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column id SqlType(int4), PrimaryKey */
+    val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
+    /** Database column hash SqlType(varchar), Length(255,true) */
+    val hash: Rep[String] = column[String]("hash", O.Length(255,varying=true))
+    /** Database column applied_at SqlType(timestamp) */
+    val appliedAt: Rep[java.sql.Timestamp] = column[java.sql.Timestamp]("applied_at")
+    /** Database column apply_script SqlType(text), Default(None) */
+    val applyScript: Rep[Option[String]] = column[Option[String]]("apply_script", O.Default(None))
+    /** Database column revert_script SqlType(text), Default(None) */
+    val revertScript: Rep[Option[String]] = column[Option[String]]("revert_script", O.Default(None))
+    /** Database column state SqlType(varchar), Length(255,true), Default(None) */
+    val state: Rep[Option[String]] = column[Option[String]]("state", O.Length(255,varying=true), O.Default(None))
+    /** Database column last_problem SqlType(text), Default(None) */
+    val lastProblem: Rep[Option[String]] = column[Option[String]]("last_problem", O.Default(None))
+  }
+  /** Collection-like TableQuery object for table PlayEvolutions */
+  lazy val PlayEvolutions = new TableQuery(tag => new PlayEvolutions(tag))
+
+  /** Entity class storing rows of table PlayEvolutionsLock
+   *  @param lock Database column lock SqlType(int4), PrimaryKey */
+  case class PlayEvolutionsLockRow(lock: Int)
+  /** GetResult implicit for fetching PlayEvolutionsLockRow objects using plain SQL queries */
+  implicit def GetResultPlayEvolutionsLockRow(implicit e0: GR[Int]): GR[PlayEvolutionsLockRow] = GR{
+    prs => import prs._
+    PlayEvolutionsLockRow(<<[Int])
+  }
+  /** Table description of table play_evolutions_lock. Objects of this class serve as prototypes for rows in queries. */
+  class PlayEvolutionsLock(_tableTag: Tag) extends profile.api.Table[PlayEvolutionsLockRow](_tableTag, "play_evolutions_lock") {
+    def * = lock <> (PlayEvolutionsLockRow, PlayEvolutionsLockRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = (Rep.Some(lock)).shaped.<>(r => r.map(_=> PlayEvolutionsLockRow(r.get)), (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column lock SqlType(int4), PrimaryKey */
+    val lock: Rep[Int] = column[Int]("lock", O.PrimaryKey)
+  }
+  /** Collection-like TableQuery object for table PlayEvolutionsLock */
+  lazy val PlayEvolutionsLock = new TableQuery(tag => new PlayEvolutionsLock(tag))
+
+  /** Entity class storing rows of table Tasks
+   *  @param id Database column id SqlType(bigserial), AutoInc, PrimaryKey
+   *  @param name Database column name SqlType(text)
+   *  @param detail Database column detail SqlType(text), Default(None)
+   *  @param userId Database column user_id SqlType(int8) */
+  case class TasksRow(id: Long, name: String, detail: Option[String] = None, userId: Long)
+  /** GetResult implicit for fetching TasksRow objects using plain SQL queries */
+  implicit def GetResultTasksRow(implicit e0: GR[Long], e1: GR[String], e2: GR[Option[String]]): GR[TasksRow] = GR{
+    prs => import prs._
+    TasksRow.tupled((<<[Long], <<[String], <<?[String], <<[Long]))
+  }
+  /** Table description of table tasks. Objects of this class serve as prototypes for rows in queries. */
+  class Tasks(_tableTag: Tag) extends profile.api.Table[TasksRow](_tableTag, "tasks") {
+    def * = (id, name, detail, userId) <> (TasksRow.tupled, TasksRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = ((Rep.Some(id), Rep.Some(name), detail, Rep.Some(userId))).shaped.<>({r=>import r._; _1.map(_=> TasksRow.tupled((_1.get, _2.get, _3, _4.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column id SqlType(bigserial), AutoInc, PrimaryKey */
+    val id: Rep[Long] = column[Long]("id", O.AutoInc, O.PrimaryKey)
+    /** Database column name SqlType(text) */
+    val name: Rep[String] = column[String]("name")
+    /** Database column detail SqlType(text), Default(None) */
+    val detail: Rep[Option[String]] = column[Option[String]]("detail", O.Default(None))
+    /** Database column user_id SqlType(int8) */
+    val userId: Rep[Long] = column[Long]("user_id")
+
+    /** Foreign key referencing Users (database name tasks_user_id_fkey) */
+    lazy val usersFk = foreignKey("tasks_user_id_fkey", userId, Users)(r => r.id, onUpdate=ForeignKeyAction.NoAction, onDelete=ForeignKeyAction.NoAction)
+  }
+  /** Collection-like TableQuery object for table Tasks */
+  lazy val Tasks = new TableQuery(tag => new Tasks(tag))
+
+  /** Entity class storing rows of table Users
+   *  @param id Database column id SqlType(bigserial), AutoInc, PrimaryKey
+   *  @param name Database column name SqlType(text) */
+  case class UsersRow(id: Long, name: String)
+  /** GetResult implicit for fetching UsersRow objects using plain SQL queries */
+  implicit def GetResultUsersRow(implicit e0: GR[Long], e1: GR[String]): GR[UsersRow] = GR{
+    prs => import prs._
+    UsersRow.tupled((<<[Long], <<[String]))
+  }
+  /** Table description of table users. Objects of this class serve as prototypes for rows in queries. */
+  class Users(_tableTag: Tag) extends profile.api.Table[UsersRow](_tableTag, "users") {
+    def * = (id, name) <> (UsersRow.tupled, UsersRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = ((Rep.Some(id), Rep.Some(name))).shaped.<>({r=>import r._; _1.map(_=> UsersRow.tupled((_1.get, _2.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column id SqlType(bigserial), AutoInc, PrimaryKey */
+    val id: Rep[Long] = column[Long]("id", O.AutoInc, O.PrimaryKey)
+    /** Database column name SqlType(text) */
+    val name: Rep[String] = column[String]("name")
+  }
+  /** Collection-like TableQuery object for table Users */
+  lazy val Users = new TableQuery(tag => new Users(tag))
+}
