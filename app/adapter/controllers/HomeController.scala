9657ddc2d77ee10f618a74a791d04eb865726272
yu-croco <yuki.kita22@gmail.com>
2020-08-16 14:13:11 +0900

    add formatter

--- app/adapter/controllers/HomeController.scala
+++ app/adapter/controllers/HomeController.scala
@@ -4,18 +4,18 @@ import javax.inject._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action { implicit request: Request[AnyContent] =>
     Ok("request ok!")
   }
