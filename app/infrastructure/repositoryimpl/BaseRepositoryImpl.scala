a0b6591ec9330672d535ce1e37093634d8bfb8a8
yu-croco <yuki.kita22@gmail.com>
2020-08-17 15:26:26 +0900

    add ops

--- app/infrastructure/repositoryimpl/BaseRepositoryImpl.scala
+++ app/infrastructure/repositoryimpl/BaseRepositoryImpl.scala
@@ -1,12 +1,13 @@
 package infrastructure.repositoryimpl
 
 import com.google.inject.Inject
+import infrastructure.helper.ops.rowpos.RowOps
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.PostgresProfile
 
 import scala.concurrent.ExecutionContext
 
-trait BaseRepositoryImpl extends HasDatabaseConfigProvider[PostgresProfile] {
+trait BaseRepositoryImpl extends HasDatabaseConfigProvider[PostgresProfile] with RowOps {
   @Inject() private var ecc: ExecutionContext = _
   implicit lazy val ec: ExecutionContext      = ecc
 
