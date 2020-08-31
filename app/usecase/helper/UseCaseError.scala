f36b885194ab3fb6a16c7b2fb5ea7a2cb490233b
yu-croco <yuki.kita22@gmail.com>
2020-08-31 21:39:50 +0900

    remove unused logic

--- app/usecase/helper/UseCaseError.scala
+++ app/usecase/helper/UseCaseError.scala
@@ -2,9 +2,7 @@ package usecase.helper
 
 import cats.data.NonEmptyList
 
-case class UseCaseError(error: NonEmptyList[(String, String)]) extends Throwable {
-  def ++(other: UseCaseError): UseCaseError = this.copy(error = this.error ::: other.error)
-}
+case class UseCaseError(error: NonEmptyList[(String, String)]) extends Throwable
 
 object UseCaseError {
   def apply(key: String, message: String): UseCaseError = UseCaseError(NonEmptyList.one(key -> message))
